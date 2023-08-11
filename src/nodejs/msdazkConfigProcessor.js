e6c52d902e1ca8dfd075a33c9806ac0457f948e4
xp2014 <p.xiong@f5.com>
2023-08-12 00:14:05 +0800

    Update config will not delete the pool unless the pool name has changed.

--- src/nodejs/msdazkConfigProcessor.js
+++ src/nodejs/msdazkConfigProcessor.js
@@ -18,10 +18,12 @@
   let blockInstance = {
     name: "instanceName", // a block instance of the iapplx config
     state: "polling", // can be "polling" for normal running state; "update" to modify the iapplx config
+    bigipPoolChange: false, // Add a signal for bigip pool change update
     bigipPool: "/Common/samplePool"
   }
   Updated by Ping Xiong on Jan/07/2023, add applicationType dropdown list, parse host(ip:port) from DCITS dubbo url
   Mar/09/2023, update delta of pool members instead of replace-all-with latest config. Updated by Ping Xiong.
+  Aug/11/2023, updated by Ping Xiong, update config will not delete the pool unless the pool changed.
 */
 
 'use strict';
@@ -186,9 +188,11 @@ msdazkConfigProcessor.prototype.onPost = function (restOperation) {
         logger.fine(
             "MSDA: onPost, " +
             instanceName +
-            " found the pool, no need to create an initial empty pool."
+            " found the pool, no need to create an initial empty pool, but update the pool configure."
         );
-        return;
+        let inputExistingPoolConfig = inputPoolName + ' monitor ' + inputMonitor + ' load-balancing-mode ' + inputPoolType;
+        let commandModifyPool = 'tmsh -a modify ltm pool ' + inputExistingPoolConfig;
+        return mytmsh.executeCommand(commandModifyPool);
     }, function (error) {
         logger.fine(
             "MSDA: onPost, " +
@@ -233,11 +237,17 @@ msdazkConfigProcessor.prototype.onPost = function (restOperation) {
     let blockInstance = {
         name: instanceName,
         bigipPool: inputPoolName,
+        // Add signal for bigip pool change update
+        bigipPoolChange: false,
         state: "polling"
     };
 
     let signalIndex = global.msdazkOnPolling.findIndex(instance => instance.name === instanceName);
     if (signalIndex !== -1) {
+        //Already has the instance, set the pool change signal if the pool changed
+        blockInstance.bigipPoolChange =
+            global.msdazkOnPolling[signalIndex].bigipPool !== inputPoolName;
+
         // Already has the instance, change the state into "update"
         global.msdazkOnPolling.splice(signalIndex, 1);
         blockInstance.state = "update";
@@ -652,35 +662,55 @@ msdazkConfigProcessor.prototype.onPost = function (restOperation) {
                     inputServiceName
                 );
             });
-            // Delete pool configuration in case it still there.
-            setTimeout (function () {
-                const commandDeletePool = 'tmsh -a delete ltm pool ' + inputPoolName;
-                mytmsh.executeCommand(commandDeletePool)
-                .then (function () {
+            // Delete pool configuration if the pool name changed.
+
+            if (
+                global.msdazkOnPolling.some(
+                    (instance) => instance.name === instanceName
+                )
+            ) {
+                let signalIndex = global.msdazkOnPolling.findIndex(
+                    (instance) => instance.name === instanceName
+                );
+                if (global.msdazkOnPolling[signalIndex].bigipPoolChange === true) {
                     logger.fine(
-                        "MSDA: onPost/stopping, " +
+                    "MSDA: onPost, " +
                         instanceName +
-                        " the pool removed: " +
-                        inputPoolName
+                        " BigipPool Changed, will delete previous pool for : ",
+                    inputServiceName
                     );
-                })
-                    // Error handling
-                .catch(function (err) {
-                    logger.fine(
-                        "MSDA: onPost/stopping, " +
-                        instanceName +
-                        " Delete failed: " +
-                        inputPoolName,
-                        err.message
-                    );
-                }).done(function () {
-                    return logger.fine(
-                        "MSDA: onPost/stopping, " +
-                        instanceName +
-                        " exit loop."
-                    );
-                });
-            }, 2000);
+
+                    setTimeout (function () {
+                        const commandDeletePool = 'tmsh -a delete ltm pool ' + inputPoolName;
+                        mytmsh.executeCommand(commandDeletePool)
+                        .then (function () {
+                            logger.fine(
+                                "MSDA: onPost/stopping, " +
+                                instanceName +
+                                " the pool removed: " +
+                                inputPoolName
+                            );
+                        })
+                            // Error handling
+                        .catch(function (err) {
+                            logger.fine(
+                                "MSDA: onPost/stopping, " +
+                                instanceName +
+                                " Delete failed: " +
+                                inputPoolName,
+                                err.message
+                            );
+                        }).done(function () {
+                            global.msdazkOnPolling[signalIndex].bigipPoolChange = false;
+                            return logger.fine(
+                                "MSDA: onPost/stopping, " +
+                                instanceName +
+                                " exit loop."
+                            );
+                        });
+                    }, 2000);
+                };
+            }
         }
     })();
 };
